37b6e07f9ca033bccef049df6bb8d60ea821bfa9
Fatma <fatmadesoky2007@gmail.com>
2026-09-09 19:43:21 +0300

    feat(models): create City schema definition

---
+++ backend/src/models/City.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const citySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "City name is required"],
+      trim: true,
+      unique: true,
+    },
+    description: {
+      type: String,
+      required: [true, "Description is required"],
+    },
+    image: {
+      type: String,
+      required: [true, "Image URL is required"],
+    },
+    region: {
+      type: String,
+      default: "Egypt",
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("City", citySchema);
